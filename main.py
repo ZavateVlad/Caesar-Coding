bdeaaa2793006cc6a93a09f545401e2c9d87f9b8
ZavateVlad <95172779+ZavateVlad@users.noreply.github.com>
2024-02-10 11:47:11 +0000

    Deleted unnecessary prints

--- main.py
+++ main.py
@@ -6,55 +6,56 @@ alphabet = list(ascii_letters)
 symbols = list(punctuation) + list(digits) + list(whitespace)
 should_end = False
 
-print(len(symbols))
-print(len(alphabet))
 #create function, which take:
 #plain_text represents the input text of the user
 #shift_amount is by how much words the user would like to shift the text
 #direction_input can be either encode or decode
 
+
 def caesar(plain_text, shift_amount, direction_input):
-  #the output of the function
-  cipher_text = ''
+    #the output of the function
+    cipher_text = ''
 
-  #loop through the input text
-  for letter in plain_text:
+    #loop through the input text
+    for letter in plain_text:
 
-    #check for word
-    if letter in alphabet:
-      alphabet_index = alphabet.index(letter)
-      
-      if direction_input == 'encode':
-        new_position = (alphabet_index + shift_amount) % 26
-      if direction_input == 'decode':
-       new_position = (alphabet_index - shift_amount) % 26
+        #check for word
+        if letter in alphabet:
+            alphabet_index = alphabet.index(letter)
 
-      new_letter = alphabet[new_position]
+            if direction_input == 'encode':
+                new_position = (alphabet_index + shift_amount) % 26
+            if direction_input == 'decode':
+                new_position = (alphabet_index - shift_amount) % 26
 
-    #check for symbols
-    elif letter in symbols:
-      symbols_index = symbols.index(letter)
-      
-      if direction_input == 'encode':
-        new_position = (symbols_index + shift_amount) % 24
-      if direction_input == 'decode':
-       new_position = (symbols_index - shift_amount) % 24
-      
-      new_letter = symbols[new_position]
-        
-    cipher_text += new_letter
-  print(f"The {direction_input}d text is: {cipher_text}")
+            new_letter = alphabet[new_position]
+
+        #check for symbols
+        elif letter in symbols:
+            symbols_index = symbols.index(letter)
+
+            if direction_input == 'encode':
+                new_position = (symbols_index + shift_amount) % 24
+            if direction_input == 'decode':
+                new_position = (symbols_index - shift_amount) % 24
+
+            new_letter = symbols[new_position]
+
+        cipher_text += new_letter
+    print(f"The {direction_input}d text is: {cipher_text}")
+
 
 #User input
 while not should_end:
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n")
-  shift = int(input("Type the shift number:\n"))
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n")
+    shift = int(input("Type the shift number:\n"))
 
-  caesar(text, shift, direction)
+    caesar(text, shift, direction)
 
-  #Restart the encryption/decryption
-  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
-  if restart == "no":
-    should_end = True
-    print("Goodbye")
+    #Restart the encryption/decryption
+    restart = input(
+        "Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
